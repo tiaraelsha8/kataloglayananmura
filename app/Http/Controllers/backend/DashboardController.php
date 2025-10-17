ac7423328cf57b1481a0a6d97db92d80561dbd6f
ArLynx <anggaramadani123@gmail.com>
2025-10-17 11:02:11 +0700

    edit statik dashboard

--- app/Http/Controllers/backend/DashboardController.php
+++ app/Http/Controllers/backend/DashboardController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers\backend;
 
+use App\Helpers\VisitorCounter;
 use App\Http\Controllers\Controller;
+use App\Models\Kategori;
+use App\Models\Layanan;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index() 
     {
-        return view('backend.dashboard');
+        $jumlahkategori = Kategori::count();
+        $jumlahlayanan = Layanan::count();
+        $statistik = VisitorCounter::count();
+        return view('backend.dashboard', compact('jumlahkategori','jumlahlayanan','statistik'));
     }
 }
